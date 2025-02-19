27b133650e44de74dcb29fe2bc01241b32aba695
sihyunjojo <si4018@naver.com>
2025-02-19 16:35:19 +0900

    feat: 초기화 로직 추가

--- src/main/java/com/sihyun/pingpong/PingpongApplication.java
+++ src/main/java/com/sihyun/pingpong/PingpongApplication.java
@@ -1,7 +1,12 @@
 package com.sihyun.pingpong;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sihyun.pingpong.dto.init.InitRequestDto;
+import com.sihyun.pingpong.service.InitService;
 
 @SpringBootApplication
 public class PingpongApplication {
@@ -10,4 +15,10 @@ public class PingpongApplication {
 		SpringApplication.run(PingpongApplication.class, args);
 	}
 
+	// @Bean
+	// public CommandLineRunner initData(InitService initService) {
+	// 	return args -> {
+	// 		initService.initializeDatabase(new InitRequestDto(1, 10));
+	// 	};
+	// }
 }
