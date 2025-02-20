a40c7b312c55097c55abfbd3c703a4eb942e7be6
sihyunjojo <si4018@naver.com>
2025-02-20 22:42:08 +0900

    refactor: Delete 함수 sql 직접 작성하여 변경

--- src/main/java/com/sihyun/pingpong/repository/UserRoomRepository.java
+++ src/main/java/com/sihyun/pingpong/repository/UserRoomRepository.java
@@ -7,6 +7,8 @@ import com.sihyun.pingpong.domain.UserRoom;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRoomRepository extends JpaRepository<UserRoom, Long> {
         boolean existsByUser(User user);
@@ -14,4 +16,8 @@ public interface UserRoomRepository extends JpaRepository<UserRoom, Long> {
         long countByRoom(Room room);
         Optional<UserRoom> findByUserAndRoom(User user, Room room);
         void deleteByRoom(Room room);
+
+        @Modifying // 기본적으로 @Query는 조회(SELECT) 쿼리만 실행할 수 있습니다.
+        @Query("DELETE FROM UserRoom ur WHERE ur.room.id = :roomId")
+        void deleteByRoomId(Long roomId);
 }
